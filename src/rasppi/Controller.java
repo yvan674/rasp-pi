4e08f017c4ca73f7af6d604eb4abead7723e0f55
yvan674 <y_satyawan@hotmail.com>
2017-10-04 00:22:09 -0500

    Layout Complete
    Layout is now complete. Now to make the contents work...

--- src/rasppi/Controller.java
+++ src/rasppi/Controller.java
@@ -1,8 +1,49 @@
 package rasppi;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+
+import java.util.Arrays;
+
 /**
  * Created by Yvan on 3.10.17.
  * Copyright (C) 2017.
  */
 public class Controller {
+    @FXML private Button expandButton;
+    @FXML private Pane rightPane;
+    @FXML public static StringProperty time = new SimpleStringProperty("00:00");
+    private static boolean pressedState = false;
+
+    public static StringProperty timeProperty(){
+        return time;
+    }
+    public String getTime(){
+        return timeProperty().get();
+    }
+    public static void setTime(String newString) {
+        timeProperty().set(newString);
+    }
+
+    @FXML
+    protected void expandButtonPressed (ActionEvent event) {
+        // sets the current state
+        pressedState = !pressedState;
+
+        // if it's pressed, then unpress. Else, press it in
+        if (pressedState) {
+            expandButton.getTransforms().add(new Rotate(180, 25, 25));
+            rightPane.getTransforms().add(new Translate(-100, 0));
+        } else {
+            expandButton.getTransforms().remove(0);
+            rightPane.getTransforms().remove(0);
+        }
+    }
 }
--- src/rasppi/Main.java
+++ src/rasppi/Main.java
@@ -1,10 +1,14 @@
 package rasppi;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.time.LocalTime;
 
 /**
  * Created by Yvan on 3.10.17.
@@ -13,6 +17,8 @@ import javafx.stage.Stage;
  * Main state machine for the program
  */
 public class Main extends Application {
+    private boolean running;
+
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Clock_fxml.fxml"));
@@ -22,8 +28,44 @@ public class Main extends Application {
         stage.setTitle("Rasp Pi");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent arg0) {
+                running = false;
+                stage.close();
+            }
+        });
+
+        runClock();
     }
 
+    private void runClock() {
+        boolean running = true;
+        new Thread() {
+            public void run() {
+                long last = System.nanoTime();
+
+                while (running) {
+                    double delta = 0;
+                    long now = System.nanoTime();
+                    delta += (now - last) / 1000000000.0;
+                    last = now;
+
+                    while (delta >= 1) {
+                        int[] time = new int[2];
+                        time[0] = LocalTime.now().getHour();
+                        time[1] = LocalTime.now().getMinute();
+                        // time to string
+                        StringBuilder build = new StringBuilder();
+                        build.append(time[0]);
+                        build.append(":");
+                        build.append(time[1]);
+                        Controller.setTime(build.toString());
+                        System.out.println(build.toString());
+                    }
+                }
+            }
+        }.start();
+    }
     public static void main(String[] args) {
         launch(args);
     }
